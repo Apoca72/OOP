9c23685b069759305deb927371787b10a2783926
Temuulen Sugar <that.u.sir.name@gmail.com>
2025-04-13 18:49:06 +0800

    Folders updated

---
+++ Lab8/this.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+class Player {
+private:
+    string name;
+public:
+    // Бүх үйлдлийг constructor дотор хийж байна
+    Player(string name) {
+        this->name = name; // 'this' нь тухайн объектын name хувьсагчийг зааж байна
+        cout << "Player created: " << this->name << endl;
+    }
+};
+
+int main() {
+    Player p("Temka");
+    return 0;
+}
