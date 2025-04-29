0d317be78c012fafc45d5586d06fd044501f6d9a
tseren717 <jvneejk@gmail.com>
2025-04-29 11:15:51 +0800

    Update Division.h

--- Lab9/Division.h
+++ Lab9/Division.h
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
-#ifndef DIVISION_H 
-#define DIVISION_H 
-#include <string> 
-using namespace std; 
- 
-class Division{ 
-    private: 
-    string divisionName; 
-    public: 
-    Division(); 
-    Division(const string& name); 
-    string getDivisionName() const; 
-    void setDivisionName(const string& name); 
-    friend ostream& operator<<(ostream& os, const Division& division); 
-}; 
-#endif 
- 
-=======
 #ifndef DIVISION_H
 #define DIVISION_H
 
 #include <string>
+#include <iostream>
 using namespace std;
 
 class Division {
@@ -28,10 +10,12 @@ private:
     string divisionName;
 
 public:
-    Division(string name);
+    Division();
+    Division(const string& name);
     string getDivisionName() const;
-    void setDivisionName(string name);
+    void setDivisionName(const string& name);
+    
+    friend ostream& operator<<(ostream& os, const Division& division);
 };
 
 #endif
->>>>>>> 36e62062b213de10188b35a9c28fd1a8c2b3f747
